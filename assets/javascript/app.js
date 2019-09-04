6f0abb69a9de49bd253156f2a56526d1e0a9162b
KuTu <haamraah@yahoo.com>
2019-09-04 02:27:17 -0700

    add save! button using localStorage ,save & UNsave event handler,showSaved(),show saved button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,6 +17,7 @@ var topics = [
     index: 0
   }
 ]
+
 // //////////
 // functions
 // //////////
@@ -73,12 +74,14 @@ function displayGifs() {
       // id is dynamiclly generated for each CARD
       var cardDiv = $("<div>").addClass("card bg-dark  m-1 float-left").attr("id", "div-" + response.data[i].id).attr("style", "width:310px ; min-width: 310px");
       // id is dynamiclly generated for each image , save the still and playing url from api in to src-still and src-play attribute, assign GIF class for event handler
-      var newImg = $("<img>").attr("id", response.data[i].id).addClass("card-img-top GIF").attr("style", "max-height: 200px ;height :200px;width:310px ; min-width: 310px ").attr("src-still", response.data[i].images.fixed_height_still.url).attr("src-play", response.data[i].images.fixed_height.url).attr("src", response.data[i].images.fixed_height_still.url);
-      var cardBody = $("<div>").addClass("card-body text-info").attr("style", "background: rgb(0,0,0);height :120px");
+      var newImg = $("<img>").attr("id", response.data[i].id).addClass("card-img-top GIF").attr("style", "max-height: 200px ;height :200px;width:310px ; min-width: 310px ").attr("src-still", response.data[i].images.fixed_height_still.url).attr("src-play", response.data[i].images.fixed_height.url).attr("src", response.data[i].images.fixed_height_still.url).attr("rating", response.data[i].rating).attr("title", response.data[i].title);
+      var cardBody = $("<div>").addClass("card-body text-info").attr("style", "background: rgb(0,0,0);height :160px");
+      var newlink = $("<a>").addClass("save btn btn-outline-info").text("♡").attr("image-id", response.data[i].id);
+
       // grab gif Title and Rating from API
       var newH = $("<h5>").addClass("card-title").text(topic + " : " + response.data[i].title);
       var newP = $("<p>").addClass("card-text").text("Rating : " + response.data[i].rating);
-      cardBody.append(newH, newP);
+      cardBody.append(newlink, newH, newP);
       cardDiv.append(newImg, cardBody);
       // show new card on top
       $("#buttom").prepend(cardDiv);
@@ -88,26 +91,50 @@ function displayGifs() {
 function playGif(img) {
   // set image src to src-play attr , we saved playing and still image url before , also add stopGIF class for event handler
   img.attr("src", img.attr("src-play")).attr("class", "card-img-top stopGIF ");
-  // remove still image and prepend moving GIF
-  $("#" + img.attr("id")).remove();
-  $("#div-" + img.attr("id")).prepend(img);
+
 }
 function stopGif(img) {
   // set image src to src-play attr , we saved playing and still image url before , also add stopGIF class for event handler
   img.attr("src", img.attr("src-still")).attr("class", "card-img-top GIF");
-  // remove still image and prepend moving GIF
-  $("#" + img.attr("id")).remove();
-  $("#div-" + img.attr("id")).prepend(img);
+
 }
+
+
+function showSaved() {
+  // read localStorage and save it in obj, then we read each key in the obj object 
+  var obj = localStorage;
+  Object.keys(obj).forEach(function (key) {
+    // for each key we have a json , use parse for convert json to js object 
+    var element = JSON.parse(localStorage.getItem(key))
+    // 
+    var cardDiv = $("<div>").addClass("card bg-dark  m-1 float-left").attr("id", "div-" + element.id).attr("style", "width:310px ; min-width: 310px");
+    // id is dynamiclly generated for each image , save the still and playing url from api in to src-still and src-play attribute, assign GIF class for event handler
+    var newImg = $("<img>").attr("id", element.id).addClass("card-img-top GIF").attr("style", "max-height: 200px ;height :200px;width:310px ; min-width: 310px ").attr("src-still", element.srcStill).attr("src-play", element.srcPlay).attr("src", element.srcStill).attr("rating", element.rating).attr("title", element.title);
+    var cardBody = $("<div>").addClass("card-body text-info").attr("style", "background: rgb(0,0,0);height :160px");
+    var newlink = $("<a>").addClass("UNsave btn btn-outline-info").text("♥").attr("image-id", element.id);
+
+    // grab gif Title and Rating from API
+    var newH = $("<h5>").addClass("card-title").text("Saved : " + element.title);
+    var newP = $("<p>").addClass("card-text").text("Rating : " + element.rating);
+    cardBody.append(newlink, newH, newP);
+    cardDiv.append(newImg, cardBody);
+    // show new card on top
+    $("#buttom").prepend(cardDiv);
+
+  });
+}
+// /////////
+// event handler
+// /////////
+
 $(document).on("click", ".topic", displayGifs);
 $(document).on("click", ".GIF", function () {
   // get an instant of the image clicked on
   var newImg = $(this);
+
   // set image src to loading.gif
   newImg.attr("src", "assets\\images\\loading.gif").attr("class", "card-img-top ");
-  // remove still image and prepend moving GIF
-  $("#" + newImg.attr("id")).remove();
-  $("#div-" + newImg.attr("id")).prepend(newImg);
+
   // after time-out call playGif with image clicked on instant (to save id and image container id)
   setTimeout(function () { playGif(newImg) }, 500);
 });
@@ -116,9 +143,7 @@ $(document).on("click", ".stopGIF", function () {
   var newImg = $(this);
   // set image src to loading gif
   newImg.attr("src", "assets\\images\\loading.gif").attr("class", "card-img-top ");
-  // remove still image and prepend moving GIF
-  $("#" + newImg.attr("id")).remove();
-  $("#div-" + newImg.attr("id")).prepend(newImg);
+
   // after time-out call stopGif with image clicked on instant (to save id and image container id)
   setTimeout(function () { stopGif(newImg) }, 500);
 });
@@ -132,6 +157,7 @@ $(document).on("click", "#clear", function () {
   // re render buttons
   renderButtons();
 });
+$(document).on("click", "#show-saved", showSaved);
 $(document).on("click", "#add-cat", function () {
   // make sure input is not empty
   if ($("#input").val().trim()) {
@@ -155,7 +181,29 @@ $(document).on("click", "#add-cat", function () {
     }
   }
 });
+$(document).on("click", ".UNsave", function () {
+  $(this).text("♡").attr("class", "save btn btn-outline-info");
+  //  console.log(localStorage);
+
+  localStorage.removeItem($(this).attr("image-id"));
+
+});
+$(document).on("click", ".save", function () {
+  $(this).text("♥").attr("class", "UNsave btn btn-outline-info");
+  var obj = new Object;
+  var image = $("#" + $(this).attr("image-id"));
+  obj.srcStill = image.attr("src-still");
+  obj.srcPlay = image.attr("src-play");
+  obj.id = image.attr("id");
+  obj.rating = image.attr("rating");
+  obj.title = image.attr("title");
+  localStorage.setItem(obj.id, JSON.stringify(obj));
+
+  // console.log(obj)
+});
 // rendering initial 
 $(document).ready(function () {
   renderButtons();
+  showSaved();
 });
+
