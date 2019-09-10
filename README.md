b15cceba5628ec425ce29ce71ee09a40c2feaeeb
KuTu <haamraah@yahoo.com>
2019-09-10 15:52:42 -0700

    add remove topic,save topics in localeStorage,pushSaved function, readme file

--- README.md
+++ README.md
@@ -1,29 +1,54 @@
 # API-GifTastick
 
-## Assignment: 
+# Assignment: 
+
 In this assignment, we'll use the GIPHY API to make a dynamic web page that populates with gifs of our choice. To finish this task, we must call the GIPHY API and use JavaScript and jQuery to change the HTML of our site.
 
-
 ## Getting Started
-View here: https://haamraah.github.io/API-GifTastick/
 
-### Screen Shots
+* View here: https://haamraah.github.io/API-GifTastick/
+* github repository : https://github.com/haamraah/API-GifTastick
 
+## Features
+####  - Search Feature
+* Give users a handy search tool so that they can find the content they need quickly.
+* users can **add** and **remove** topics, topics are **saved** in **localStorage** .
+* add topic button is limited to Not add duplicate topic or empty text topic
+* each topic shows the numbre of GIFs related on the screen
+#### - Less Keyboarding and More Touch
+* once user adds the topic , each time user clicks on a topic i'll add maximum of 4 new GIFs to screen
+* each time user clicks on a GIF i'll **start or stop** GIF based on actual state
+#### - Save Favorite GIFs
+* clicking on the ♥ button on each GIF you can **add** to saved GIFs or **remove** from **saved GIFs**
+* clicking on the ♥ button on top of the page you will see the saved GIFs , that button also **shows if the saved GIFs is empty**
+* **Saved GIFs** are saved on **localStorage**
+#### - Responsive Design for Varying Screen Sizes
 
 
 
 
 ## Technologies used
-- Bootstrap
-- jQuery
+
+* Bootstrap
+* jQuery
 
 ## Prerequisites
-- Bootstrap: visit http://getbootstrap.com/getting-started/ for the CDN and link in html
-- jQuery: visit http://code.jquery.com/ and link in html
+
+* Bootstrap: visit http://getbootstrap.com/getting-started/ for the CDN and link in html
+* jQuery: visit http://code.jquery.com/ and link in html
 
 ## Built With
-- Sublime Text - Text Editor
-- Bootstrap- Wireframe
 
-## Author
+* Sublime Text - Text Editor
+* Bootstrap- Wireframe
+
+### Author
+
 Hamrah - HTML/CSS/JS
+
+
+### Screen Shots
+
+![screen shot](assets/images/screenShot01.jpg)
+
+![screen shot](assets/images/screenShot02.jpg)
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var topics = [
+var saveTopics = [
   {
     name: "Uma Thurman",
     index: 0
@@ -16,7 +16,11 @@ var topics = [
     index: 0
   }
 ]
+if (!alreadySaved("topics")) {
+  localStorage.setItem("topics", JSON.stringify(saveTopics));
+}
 
+var topics = JSON.parse(localStorage.getItem("topics"))
 // //////////
 // functions
 // //////////
@@ -27,21 +31,32 @@ function renderButtons() {
   $("#top").empty();
   // Loops through the array of topics
   for (var i = 0; i < topics.length; i++) {
-
     // Then dynamicaly generates buttons for each topic in the array
     // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
     var a = $("<button>");
     // Adds a class of .topic to our button
-    a.addClass("topic btn btn-outline-info m-1");
+    a.addClass("topic btn btn-outline-info m-0 m-r-0 p-1");
     // Added a data-attribute
     a.attr("data-name", topics[i].name);
     //  added a startAt index
     a.attr("startAt", topics[i].index);
-
     // Provided the initial button text
     a.text(topics[i].name + " +" + topics[i].index);
-    // Added the button to the top div
-    $("#top").append(a);
+
+    // create a close (X) button
+    var removeBtn = $("<button>");
+    removeBtn.addClass("removeBtn btn btn-outline-info border-0 p-0 m-0 mr-3");
+    // Added a data-attribute
+    removeBtn.attr("data-name", topics[i].name);
+    removeBtn.attr("style", "font-size:20px; ");
+    // Provided the initial button text
+    removeBtn.text("⮽");
+    // Added the X button to top div
+    // Added the topic button to the top div
+    $("#top").append(a, removeBtn);
+
+
+
 
   }
 }
@@ -86,7 +101,8 @@ function displayGifs() {
       var maskImg = $("<img>").attr("src", "assets/images/GIF.png").addClass("align-self-center");
 
       // grab gif Title and Rating from API
-      var newH = $("<p>").addClass("card-text ").text(response.data[i].title.substring(0, 18) + "... " + " Rate: " + response.data[i].rating).attr("style", "text-shadow: 2px 2px 1px #000000");
+      var newH = $("<p>").addClass("card-text ").text(`${response.data[i].title.substring(0, 18)}... 
+      Rate: ${response.data[i].rating}`).attr("style", "text-shadow: 2px 2px 1px #000000");
       // append title-rating , play button , save button
       cardBody.append(newH, maskImg, newlink);
 
@@ -120,43 +136,74 @@ function alreadySaved(id) {
 function showSaved() {
   // read localStorage and save it in obj, then we read each key in the obj object 
   var obj = localStorage;
-  console.log(Object.keys(obj))
   Object.keys(obj).forEach(function (key) {
-    // for each key we have a json , use parse for convert json to js object 
-    var element = JSON.parse(localStorage.getItem(key))
-    // 
-    var cardDiv = $("<div>").addClass("card bg-dark  m-1 float-left").attr("id", "div-" + element.id);
-    // id is dynamiclly generated for each image , save the still and playing url from api in to src-still and src-play attribute, assign GIF class for event handler
-    var newImg = $("<img>").attr("id", element.id).addClass("card-img GIF").attr("src-still", element.srcStill).attr("src-play", element.srcPlay).attr("src", element.srcStill).attr("rating", element.rating).attr("title", element.title).attr("style", "height :100%;");
+    if (key != "topics") {
+      // for each key we have a json , use parse for convert json to js object 
+      var element = JSON.parse(localStorage.getItem(key))
+      // 
+      var cardDiv = $("<div>").addClass("card bg-dark  m-1 float-left").attr("id", "div-" + element.id);
+      // id is dynamiclly generated for each image , save the still and playing url from api in to src-still and src-play attribute, assign GIF class for event handler
+      var newImg = $("<img>").attr("id", element.id).addClass("card-img GIF").attr("src-still", element.srcStill).attr("src-play", element.srcPlay).attr("src", element.srcStill).attr("rating", element.rating).attr("title", element.title).attr("style", "height :100%;");
 
-    var cardBody = $("<div>").addClass("card-img-overlay GIF d-flex align-items-end justify-content-between text-white").attr("unic-id", element.id);
-    // save button
-    var newlink = $("<a>").addClass("UNsave float-right btn btn-outline-info").text("♥").attr("image-id", element.id).attr("style", "text-shadow: 2px 2px 1px #000000");;
-    // play button
-    var maskImg = $("<img>").attr("src", "assets/images/GIF.png").addClass("align-self-center");
+      var cardBody = $("<div>").addClass("card-img-overlay GIF d-flex align-items-end justify-content-between text-white").attr("unic-id", element.id);
+      // save button
+      var newlink = $("<a>").addClass("UNsave float-right btn btn-outline-info").text("♥").attr("image-id", element.id).attr("style", "text-shadow: 2px 2px 1px #000000");;
+      // play button
+      var maskImg = $("<img>").attr("src", "assets/images/GIF.png").addClass("align-self-center");
 
-    // grab gif Title  from localStorage
-    var newH = $("<p>").addClass("card-text").text(element.title).attr("style", "max-width:40%;text-shadow: 2px 2px 1px #000000");
+      // grab gif Title  from localStorage
+      var newH = $("<p>").addClass("card-text").text(element.title).attr("style", "max-width:40%;text-shadow: 2px 2px 1px #000000");
 
-    // append title-rating , play button , save button
-    cardBody.append(newH, maskImg, newlink);
-    cardDiv.append(newImg, cardBody);
-    // show new card on top
-    $("#buttom").prepend(cardDiv);
-
+      // append title-rating , play button , save button
+      cardBody.append(newH, maskImg, newlink);
+      cardDiv.append(newImg, cardBody);
+      // show new card on top
+      $("#buttom").prepend(cardDiv);
+    }
   });
+
+
 }
 function checkSaved() {
-  // check if localStorage is empty, if
-  var obj = localStorage;
-  var saved = Object.keys(obj).length;
-  console.log(saved)
-  if (saved == 0) {
+  // // if we call checkSaved with parameter
+  // if (id) {
+  //   const obj = localStorage;
+  //   Object.keys(obj).forEach(function (obj) {
+  //     if (obj.id == id) {
+  //       return false;
+  //     } else {
+  //       return true;
+  //     }
+  //   });
+  //   // if we call checkSaved without any parameter
+  // } else if (!id) {  // check if localStorage is empty, if
+  const obj = localStorage;
+  if (Object.keys(obj).length == 1) {
     $("#show-saved").text("♡").attr("class", "btn  btn-outline-info float-right m-1")
+
   } else {
     $("#show-saved").text("♥").attr("class", "show-saved btn  btn-outline-info float-right m-1")
+
   }
+  // }
 }
+function pushSaved() {
+
+  var obj = {
+    id: "U3E2gTwAyacx2",
+    rating: "pg",
+    srcPlay: "https://media3.giphy.com/media/U3E2gTwAyacx2/giphy.gif?cid=080e98f186f59a7d35e548f3352d212c22725c9ff27417fe&rid=giphy.gif",
+    srcStill: "https://media3.giphy.com/media/U3E2gTwAyacx2/giphy_s.gif?cid=080e98f186f59a7d35e548f3352d212c22725c9ff27417fe&rid=giphy_s.gif",
+    title: "roman GIF",
+  }
+  // check if we already have roman GIF ^ in localStorage 
+  if (!alreadySaved(obj.id)) {
+    localStorage.setItem(obj.id, JSON.stringify(obj));
+  }
+
+
+}
+
 // /////////
 // event handler
 // /////////
@@ -213,7 +260,7 @@ $(document).on("click", "#add-cat", function () {
     var newTopic = $("#input").val().trim().toLowerCase();
     // make sure we do not duplicate a topic
     var notInTopics = true;
-    topics.filter(function (i) {
+    topics.forEach(i => {
       if (i.name == newTopic) {
         notInTopics = false;
       }
@@ -222,6 +269,9 @@ $(document).on("click", "#add-cat", function () {
       // The topic from the textbox is then added to our array
       var newObj = { name: newTopic, index: 0 }
       topics.push(newObj);
+
+      localStorage.setItem("topics", JSON.stringify(topics));
+
       // delete text in the text box (success)
       $("#input").val("")
       // Calling renderButtons which handles the processing of our topic array
@@ -248,11 +298,22 @@ $(document).on("click", ".save", function () {
 
   checkSaved();
 });
+$(document).on("click", ".removeBtn", function () {
+  var buttonTopic = $(this).attr("data-name");
+  topics = topics.filter(function (topic) {
+    if (topic.name !== buttonTopic) { return topic }
+  })
+  localStorage.setItem("topics", JSON.stringify(topics));
+  renderButtons();
+});
 // rendering initial 
 $(document).ready(function () {
+
   renderButtons();
-  checkSaved();
+  pushSaved();
   showSaved();
+
+  checkSaved();
   // alreadySaved("a");
 });
 
